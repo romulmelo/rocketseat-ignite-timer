a0a2ef6b8cf4e8d906dbb27f4c351eb0bbcb0915
Romulo Melo <romulo.melo@valtech.com>
2022-12-21 11:11:42 -0300

    feat(style): set default app theme to theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
