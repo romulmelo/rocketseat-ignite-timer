e5a84e0742a044c90aca3fd0bd5b095558f1ead9
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2022-12-21 21:01:17 -0300

    feat: add standard router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router";
+
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
