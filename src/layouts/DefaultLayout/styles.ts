d05c134a3f6cddb7d793dc32c85e9c972d9b5eb0
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2022-12-23 08:26:29 -0300

    feat: add default layout

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+  margin: 5rem auto;
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.color.gray[800]};
+  border-radius: ${({ theme }) => theme.radii.md};
+`;
